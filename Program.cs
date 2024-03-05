fd137244ae597c333bfe1adb9ee180da07f5bafd
ShoxruxCode <zokirjonovshoxrux99@gmail.com>
2024-03-05 21:45:39 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.Write("Enter first number: ");
+string asFirstNumber = Console.ReadLine();
+int firstNumber = Convert.ToInt32(asFirstNumber);
+Console.Write("Enter second number: ");
+string asSecondNumber = Console.ReadLine();
+int secondNumber = Convert.ToInt32(asSecondNumber);
+
+Console.WriteLine($"Adding: {firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+Console.WriteLine($"Substructing: {firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+Console.WriteLine($"Multipling: {firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+Console.WriteLine($"Deviding: {firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+Console.WriteLine($"asDeviding: {firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
