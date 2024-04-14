dbc8e2985954e3bcc26b26f661d9d23da6b2e66d
ShoxruxCode <zokirjonovshoxrux99@gmail.com>
2024-04-14 19:52:25 +0500

    INFRA : Upload Project

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 Console.Write("Enter first number: ");
 string asFirstNumber = Console.ReadLine();
@@ -6,6 +7,8 @@ int firstNumber = Convert.ToInt32(asFirstNumber);
 Console.Write("Enter second number: ");
 string asSecondNumber = Console.ReadLine();
 int secondNumber = Convert.ToInt32(asSecondNumber);
+Console.Write("Enter operations (+, -, *, /, %) : ");
+string operations = Console.ReadLine();
 if(firstNumber > secondNumber)
 {
     Console.WriteLine("first number is bigger than second number");
@@ -21,3 +24,34 @@ string message =
         ? "first number is bigger than or equal to second number" 
         : "first number is lower than second number";
 Console.WriteLine(message);
+// switch(operations)
+// {
+//     case "+" :
+//         Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+//         break;
+//     case "-" :
+//         Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+//         break;
+//     case "*" :
+//         Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+//         break;
+//     case "/" :
+//         Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+//         break;
+//     case "%" :
+//         Console.WriteLine($"{firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
+//         break;
+//     default :
+//         Console.WriteLine("Operation not found");
+//         break;
+// }
+string result = operations switch
+{
+    "+" => $"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}",
+    "-" => $"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}",
+    "*" => $"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}",
+    "/" => $"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}",
+    "%" => $"{firstNumber} % {secondNumber} = {firstNumber % secondNumber}",
+    _ => "Operation not found"
+};
+Console.WriteLine(result);
